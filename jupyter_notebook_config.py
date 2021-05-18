09cd529629999ceaee8658111124eb3293f5a5ac
Valles Marineris Hiker <dst1213@126.com>
2021-05-18 21:01:52 +0800

    add requirements and jupyterlab password

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,3 +4,4 @@
 # we have SSL certificates enabled for encryption purposes.
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
+c.NotebookApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$PFNzH66Tby2B0yCc84qvLA$rSHvdej3iRnL1x/WurT0cQ'
